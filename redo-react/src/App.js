7275f121e0eb8d52e49029b9237be37caadeac3c
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-01 15:06:33 +0200

    Add button, make it work

--- redo-react/src/App.js
+++ redo-react/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div>
-      
+      <label>
+        <input type="text" placeholder="write a new todo"/>
+        <button>Add Todo</button>
+      </label>
     </div>
   );
 }
